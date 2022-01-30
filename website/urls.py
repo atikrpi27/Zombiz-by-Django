13dd5eed203ac7e815375025832a3d08dc378e98
Md. Atikur Rahman <atik.rpi27@gmail.com>
2022-01-29 21:59:27 -0800

    Add About and Service Pages with static files

--- website/urls.py
+++ website/urls.py
@@ -6,5 +6,11 @@ from .import views
 apps_name = 'website'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('service/', views.service, name="service"),
+    path('project/', views.project, name="project"),
+    path('team/', views.team, name="team"),
+    path('blog/', views.blog, name="blog"),
+    path('contact/', views.contact, name="contact"),
 ]
--- website/views.py
+++ website/views.py
@@ -5,3 +5,21 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,"index.html")
+
+def about(request):
+    return render(request,"about.html")
+    
+def service(request):
+    return render(request,"service.html")
+
+def project(request):
+    return render(request,"project.html")
+
+def team(request):
+    return render(request,"team.html")
+
+def blog(request):
+    return render(request,"blog.html")
+
+def contact(request):
+    return render(request,"contact.html")
